116e6c8352116e446cfb377be9939734ca9bf890
Maria Gregorio <maria.gregorio@globant.com>
2020-04-16 12:35:44 -0300

    changed a few assets

--- jetpack-melvin-proto/game.py
+++ jetpack-melvin-proto/game.py
@@ -17,7 +17,7 @@ run = True
 
 class Platform:
     def __init__(self, rect):
-        self.rect = rect
+        self.rect = pygame.Rect(rect[0], rect[1], rect[2], rect[3])
 
     def draw(self):
         pygame.draw.rect(screen, (0, 255, 0), self.rect)
@@ -26,82 +26,74 @@ class Platform:
 class Melvin:
     def __init__(self):
         self.sprite = pygame.transform.scale(pygame.image.load("mel_sprite.png"), (128, 128))
-        self.gravity = 3
+        self.gravity = 4
         self.x = 0
         self.y = 50
-        self.jetpackPower = 6
+        self.jetpackPower = 8
         self.hitbox = (self.x + 36, self.y + 10, 50, 110)
         self.isFloor = False
         self.isCeiling = False
         self.xDirection = 1
+        self.yDirection = 1
         self.leftCollision = False
         self.rightCollision = False
 
     def draw(self):
         screen.blit(self.sprite, (self.x, self.y))
         self.hitbox = (self.x + 36, self.y + 10, 50, 110)
-        pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 2)
+        # pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 2)
 
     def apply_gravity(self):
         if self.isFloor is False and self.hitbox[1] + self.hitbox[3] < win_height:
             self.y += self.gravity
+            self.yDirection = 1
 
     def activate_jetpack_power(self):
+        # TODO animación del fueguitoooo
         if self.hitbox[1] > 0:
             self.y -= self.jetpackPower
+            self.yDirection = -1
 
-    def check_collision(self, platforms):
+    def check_collision_with_platforms(self, platforms):
         collided_bottom = False
         collided_top = False
         collided_left = False
         collided_right = False
-        for platform in platforms:
-            # bottom collision
-            if platform.rect[1] <= self.hitbox[1] + self.hitbox[3] <= platform.rect[1] + platform.rect[3]:
-                for point in range(self.hitbox[0], self.hitbox[0] + self.hitbox[2]):
-                    if platform.rect[0] < point < platform.rect[0] + platform.rect[2]:
-                        collided_bottom = True
-                        break
-            # top collision
-            if platform.rect[1] + platform.rect[3] >= self.hitbox[1] >= platform.rect[1]:
-                for point in range(self.hitbox[0], self.hitbox[0] + self.hitbox[2]):
-                    if platform.rect[0] < point < platform.rect[0] + platform.rect[2]:
-                        collided_top = True
-                        break
-            # left collision
-            if self.xDirection == -1 and platform.rect[0] + platform.rect[2] - 10 <= self.hitbox[0] <= \
-                    platform.rect[0] + platform.rect[2]:  # the minus ten is for some offset padding
-                for point in range(self.hitbox[1], self.hitbox[1] + self.hitbox[3]):
-                    if platform.rect[1] < point < platform.rect[1] + platform.rect[3]:
-                        collided_left = True
-                        break
-            # right collision
-            if self.xDirection == 1 and platform.rect[0] + 10 >= self.hitbox[0] + self.hitbox[2] >= platform.rect[0]:
-                # the plus ten is for some offset padding
-                for point in range(self.hitbox[1], self.hitbox[1] + self.hitbox[3]):
-                    if platform.rect[1] < point < platform.rect[1] + platform.rect[3]:
-                        collided_right = True
-                        break
-        if collided_bottom:
-            self.isFloor = True
-        else:
-            self.isFloor = False
-        if collided_top:
-            self.isCeiling = True
-        else:
-            self.isCeiling = False
-        if collided_left:
-            self.leftCollision = True
-        else:
-            self.leftCollision = False
-        if collided_right:
-            self.rightCollision = True
-        else:
-            self.rightCollision = False
+        melvin_hitbox_rect = pygame.Rect(self.hitbox[0], self.hitbox[1], self.hitbox[2], self.hitbox[3])
+        platform_collision_index = melvin_hitbox_rect.collidelist(platforms)
+        # if theres a collision, check from which side
+        if platform_collision_index != -1:
+            # bottom
+            for point in range(self.hitbox[0], self.hitbox[0] + self.hitbox[2]):
+                if platforms[platform_collision_index].collidepoint(point, self.hitbox[1] + self.hitbox[3]):
+                    collided_bottom = True
+                    break
+            # top
+            for point in range(self.hitbox[0], self.hitbox[0] + self.hitbox[2]):
+                if platforms[platform_collision_index].collidepoint(point, self.hitbox[1]):
+                    collided_top = True
+                    break
+            # right
+            for point in range(self.hitbox[1], self.hitbox[1] + self.hitbox[3]):
+                if platforms[platform_collision_index].collidepoint(self.hitbox[0] + self.hitbox[2], point):
+                    collided_right = True
+                    break
+            # left
+            for point in range(self.hitbox[1], self.hitbox[1] + self.hitbox[3]):
+                if platforms[platform_collision_index].collidepoint(self.hitbox[0], point):
+                    collided_left = True
+                    break
+
+        # TODO when standing on platform, change sprite to standing. when walking, animate
+        self.isFloor = True if collided_bottom else False
+        self.isCeiling = True if collided_top else False
+        self.rightCollision = True if collided_right and self.isFloor is False else False
+        self.leftCollision = True if collided_left and self.isFloor is False else False
 
 
 melvin = Melvin()
-platforms = [Platform((50, 450, 600, 10)), Platform((400, 200, 200, 10))]
+lvl_platforms = [Platform((50, 450, 300, 20)), Platform((300, 200, 200, 20))]
+lvl_platforms_rects = [platform.rect for platform in lvl_platforms]
 
 while run:
     for event in pygame.event.get():
@@ -114,21 +106,21 @@ while run:
         if melvin.isCeiling is False:
             melvin.activate_jetpack_power()
     if keys[pygame.K_LEFT] and melvin.leftCollision is False:
-        melvin.x -= 4
+        melvin.x -= 6
         melvin.xDirection = -1
     if keys[pygame.K_RIGHT] and melvin.rightCollision is False:
-        melvin.x += 4
+        melvin.x += 6
         melvin.xDirection = 1
 
     screen.fill(colors["almost black"])
 
-    for platform in platforms:
+    for platform in lvl_platforms:
         platform.draw()
 
     melvin.draw()
     melvin.apply_gravity()
 
-    melvin.check_collision(platforms)
+    melvin.check_collision_with_platforms(lvl_platforms_rects)
 
     pygame.display.update()
     clock.tick(30)
